a913c5fa91c22aa95b84439ed8d97601c2f62139
simplhug <simplhug@gmail.com>
2017-01-16 15:30:57 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+        System.out.println("Hello, World! This is from GitHub Web-hook.");
     }
 
 }
